93c976480b2ace62b52653b3c60e250405572990
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-06 20:34:23 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
+    Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+    Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
+    basic.showIcon(IconNames.Sad)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    texte = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (texte == "ouvert") {
+        Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
+        Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        if (texte == "ferme") {
+            Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+            Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
+            basic.showIcon(IconNames.Sad)
+        } else {
+            basic.showString(texte)
+        }
+    }
+})
+let texte = ""
+Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+basic.forever(function () {
+	
+})
