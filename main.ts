0c558d9ff175c23943f155d5dfa9d16b6bffd85d
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-06 20:36:40 +0200

    corr bug

--- main.ts
+++ main.ts
@@ -10,12 +10,12 @@ input.onButtonPressed(Button.B, function () {
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     texte = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-    if (texte == "ouvert") {
+    if (texte.includes("ouvert")) {
         Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
         Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
         basic.showIcon(IconNames.Happy)
     } else {
-        if (texte == "ferme") {
+        if (texte.includes("ferme")) {
             Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
             Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
             basic.showIcon(IconNames.Sad)
