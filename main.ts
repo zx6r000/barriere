ab0d4baa8ea1f9c631e8ef74cca47b27b7f51bbf
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-06 22:03:20 +0200

    ajout fonction + clap

--- main.ts
+++ main.ts
@@ -1,33 +1,36 @@
+input.onSound(DetectedSound.Loud, function () {
+    ouvre()
+    basic.pause(2000)
+    ferme()
+})
 input.onButtonPressed(Button.A, function () {
+    ouvre()
+})
+function ouvre () {
     Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
     Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
     basic.showIcon(IconNames.Happy)
-})
-input.onButtonPressed(Button.B, function () {
+}
+function ferme () {
     Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
     Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
     basic.showIcon(IconNames.Sad)
+}
+input.onButtonPressed(Button.B, function () {
+    ferme()
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     texte = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     if (texte.includes("ouvert")) {
-        Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
-        Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
-        basic.showIcon(IconNames.Happy)
+        ouvre()
     } else {
         if (texte.includes("ferme")) {
-            Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
-            Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
-            basic.showIcon(IconNames.Sad)
+            ferme()
         } else {
             if (texte.includes("auto")) {
-                Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
-                Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 2)
-                basic.showIcon(IconNames.Happy)
+                ouvre()
                 basic.pause(3000)
-                Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
-                Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
-                basic.showIcon(IconNames.Sad)
+                ferme()
             } else {
             	
             }
